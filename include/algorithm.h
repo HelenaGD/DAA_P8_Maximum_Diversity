126b771be7a525ab90093b5d60b00a6e80822aa3
HelenaGD <alu0100829150@ull.edu.es>
2023-04-20 20:07:43 +0000

    Algoritmo constructivo + lrc implementada

--- include/algorithm.h
+++ include/algorithm.h
@@ -9,7 +9,81 @@
 template <typename T>
 class Algorithm {
  public:
-    Algorithm() {};
-    ~Algorithm() {};
-    virtual Solution<T> run(Problem<T> problem) = 0;
+  typedef std::pair<int, T> farthestElement;
+  typedef vector<vector<T>> Cluster;
+  typedef vector<T> Punto;
+  Algorithm() {};
+  ~Algorithm() {};
+
+  /**
+   * Método para construir la solución inicial
+   * mediante un algoritmo constructivo voraz
+   * @param problem Instancia del problema
+   * @param m Número de puntos a seleccionar
+   * @return Solución inicial
+  */
+  Solution<T> constructivo_voraz(Problem<T> problem, int m = 2, int size_lrc = 1) {
+    Cluster solution; // Tendré m puntos en la solucion
+    Cluster puntos_disponibles = problem.get_points(); // Tendré todos los puntos disponibles
+    // Primero calculo el centroide de la nube total de puntos
+    vector<T> centroid = centroid(problem.get_points());
+
+    // El punto más alejado del centroide en una lrc será el primer punto de la solución
+    farthestElement choosedElement;
+    int random_index_dist = rand() % size_lrc;
+    vector<farthestElement> distances = get_lrc(puntos_disponibles, centroid, size_lrc);
+    choosedElement = distances[random_index_dist];
+    solution.push_back(puntos_disponibles[choosedElement.first]);
+    puntos_disponibles.erase(puntos_disponibles.begin() + solution[0]);
+
+    distances.clear();
+
+    // Ahora, hasta que no se alcance el número de puntos en la solucion
+    while (solution.size() < m) {
+      // Calculo el centroide de mi solución
+      centroid = centroid(solution);
+
+      // Elijo aleatoriamente entre los X puntos más alejados
+      random_index_dist = rand() % size_lrc;
+      distances = get_lrc(puntos_disponibles, solution, size_lrc);
+      choosedElement = distances[random_index_dist];
+
+      // Añado el punto elegido a los puntos de servicio
+      solution.push_back(puntos_disponibles[choosedElement.first]);
+      puntos_disponibles.erase(puntos_disponibles.begin() + solution[solution.size() - 1]); // Elimino el punto de los disponibles
+      distances.clear();
+    }
+
+    // Creo la solución
+    Solution<T> solucion(solution);
+    return solucion;
+  };
+
+  vector<farthestElement> get_lrc(Cluster puntos_disponibles, Cluster solution, int size = 3) {
+    vector<farthestElement> distances;
+    vector<farthestElement> bestKDistances;
+
+    // Calculo la distancia de todos los puntos a los servicios
+    for (int i = 0; i < puntos_disponibles.get_m(); i++) { // Para cada punto
+      T min_distance = std::numeric_limits<T>::max(); // Inicializo la distancia mínima a infinito
+      for (int j = 0; j < solution.size(); j++) { // Para cada punto de servicio
+        T distance = euclidean_distance(puntos_disponibles.get_points()[i], solution[j]); // Calculo la distancia
+        if (distance < min_distance) min_distance = distance; // Si es menor que la mínima, actualizo
+      }
+      distances.push_back(std::make_pair(i, min_distance)); // Guardo el punto y su distancia
+    }
+
+    // Ordeno los puntos por distancia más alejada
+    std::sort(distances.begin(), distances.end(), [](const farthestElement& a, const farthestElement& b) {
+      return a.second > b.second;
+    });
+
+    // Elijo los k puntos más alejados
+    for (int i = 0; i < size; i++) {
+      bestKDistances.push_back(distances[i]);
+    }
+
+    // Retorno la lrc
+    return bestKDistances;
+  }
 };
